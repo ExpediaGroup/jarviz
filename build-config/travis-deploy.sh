d431750f6599bc5e4381f00574b7790eded93c08
Lasantha Kularatne <lkularatne@expediagroup.com>
2020-03-11 11:06:54 -0500

    Ignoring deploy for mvn prepare commits

--- build-config/travis-deploy.sh
+++ build-config/travis-deploy.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' -a ! -z "$SONATYPE_NEXUS_USERNAME" ]; the
   if [ ! -z "$TRAVIS_TAG" ]; then
     mkdir -p ${GPG_DIR}
 
-    echo "Deploying jarviz-lib release"
+    echo "Deploying jarviz-lib release $TRAVIS_TAG"
     openssl aes-256-cbc -K $encrypted_a065d0ea71e2_key -iv $encrypted_a065d0ea71e2_iv -in build-config/gpg.asc.enc -out ${GPG_DIR}/gpg.asc -d
     gpg --version
     gpg --import ${GPG_DIR}/gpg.asc
@@ -18,9 +18,12 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' -a ! -z "$SONATYPE_NEXUS_USERNAME" ]; the
 
     rm -rf ${GPG_DIR}
   else
-
-    echo "Deploying jarviz-lib snapshot"
-    mvn deploy --settings build-config/travis-mvn-settings.xml -B -U -P oss-snapshot -DskipTests=true
+    if [[ "$TRAVIS_COMMIT_MESSAGE" == '[maven-release-plugin] prepare release'* ]] ; then
+      echo 'Skipping mvn deploy, the release should happend when the tag is built.'
+    else
+      echo 'Deploying jarviz-lib snapshot'
+      mvn deploy --settings build-config/travis-mvn-settings.xml -B -U -P oss-snapshot -DskipTests=true
+    fi
   fi
 fi
 
