572904501f44b50984e659220159837dffdf2751
Lasantha Kularatne <lkularatne@expediagroup.com>
2020-03-11 09:40:16 -0500

    Building node module

--- build-config/travis-build-node.sh
+++ build-config/travis-build-node.sh
@@ -5,11 +5,13 @@ set -o pipefail
 set -o nounset
 
 JAR_CUR_DIR="$(pwd)"
-
-echo 'Building jarviz-graph'
 cd ./jarviz-graph
 
+echo 'Installing jarviz-graph'
 npm install
+
+echo 'Building jarviz-graph'
+npm run build
 
 cd "$JAR_CUR_DIR"
 echo 'Done'
