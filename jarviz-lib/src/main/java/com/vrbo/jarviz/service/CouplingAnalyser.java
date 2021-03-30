64798ce6bb7f1df24193a4af0b126cef19d74477
Lasantha Kularatne <lkularatne@expediagroup.com>
2021-03-30 15:47:04 -0500

    Upgrade to ASM 9.1 & config improvements (#9)
    * Upgrade to ASM 9.1 & config improvements
    
    * Increasing default Maven timeout to 5 min

--- jarviz-lib/src/main/java/com/vrbo/jarviz/service/CouplingAnalyser.java
+++ jarviz-lib/src/main/java/com/vrbo/jarviz/service/CouplingAnalyser.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2020 Expedia, Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2020 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.vrbo.jarviz.service;
 
@@ -46,15 +46,23 @@ public class CouplingAnalyser {
     /**
      * Start the analyser and return a list of {@link MethodCoupling}s.
      *
-     * @param config         The configurations.
+     * @param jarvizConfig   The configurations.
      * @param applicationSet The application set.
      * @param filterConfig   The filters.
      * @param reportFile     File name for the report.
      */
-    public void start(final JarvizConfig config,
+    public void start(final JarvizConfig jarvizConfig,
                       final ApplicationSet applicationSet,
                       final CouplingFilterConfig filterConfig,
                       final String reportFile) {
+        final JarvizConfig config;
+        if (jarvizConfig != null) {
+            config = jarvizConfig;
+        } else {
+            log.info("JarvizConfig is not found, using default config.");
+            config = new JarvizConfig.Builder().build();
+        }
+
         init(config);
 
         final ServiceLocator serviceLocator = JarvizServiceLocator.createServiceLocator(config);
@@ -67,17 +75,10 @@ public class CouplingAnalyser {
 
     private void init(final JarvizConfig jarvizConfig) {
         log.info("Initializing analyser");
-
-        JarvizConfig effectiveConfig = jarvizConfig;
-        if (jarvizConfig == null) {
-            log.info("JarvizConfig is not found, using default config.");
-            effectiveConfig = JarvizConfig.createDefaultConfig();
-        }
-
-        final String artifactDir = effectiveConfig.getArtifactDirectory();
-        log.info("Artifact directory: {}", artifactDir);
-        if (!getOrCreateDirectory(artifactDir).exists()) {
-            throw new IllegalStateException("Failed to initiate artifact directory " + artifactDir);
+        final String dir = jarvizConfig.getArtifactDirectory();
+        log.info("Artifact directory: {}", dir);
+        if (!getOrCreateDirectory(dir).exists()) {
+            throw new IllegalStateException("Failed to initiate artifact directory " + dir);
         }
     }
 
