52bb15b04b95ba4a1f8be0d731f8a79b5423858b
Lasantha Kularatne <lkularatne@vrbo.com>
2021-03-31 16:03:53 -0500

    Vars

--- build-config/release-java.sh
+++ build-config/release-java.sh
@@ -4,26 +4,16 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-echo 'Releasing jarviz-lib...'
-
 if [ ! -z "$SONATYPE_USERNAME" ]; then
-  if [ ! -z "$TRAVIS_TAG" ]; then
-    echo "Releasing for tag $TRAVIS_TAG"
+  echo "Releasing for tag $GITHUB_REF"
 
-    echo 'Building jarviz-lib'
-    mvn clean install -DskipTests
+  echo 'Building jarviz-lib'
+  mvn clean install -DskipTests
 
-    echo 'Deploying jarviz-lib release'
-    #mvn deploy --settings build-config/mvn-settings.xml -B -U -P oss-release -DskipTests=true
-  else
-    if [[ "$TRAVIS_COMMIT_MESSAGE" == '[maven-release-plugin] prepare release'* ]] ; then
-      echo 'Skipping deploy for release preparation step.'
-      exit 0
-    fi
+  echo 'Releasing jarviz-lib'
+  #mvn deploy --settings build-config/mvn-settings.xml -B -U -P oss-release -DskipTests=true
 
-    echo 'Releasing jarviz-lib snapshot'
-    #mvn deploy --settings build-config/mvn-settings.xml -B -U -P oss-snapshot -DskipTests=true
-  fi
+  #mvn deploy --settings build-config/mvn-settings.xml -B -U -P oss-snapshot -DskipTests=true
 else
   echo 'Sonatype authentication variables missing!'
 fi
