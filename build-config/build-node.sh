43254ed52a2649cc1a7691b8248f0268b639d7e9
Lasantha Kularatne <lkularatne@vrbo.com>
2021-03-31 19:39:47 -0500

    Fixing build scripts to ignore prepare steps

--- build-config/build-node.sh
+++ build-config/build-node.sh
@@ -5,8 +5,7 @@ set -o pipefail
 set -o nounset
 
 LAST_COMMIT_MSG=$(git log -1 --pretty=format:"%s")
-LAST_COMMIT_TAG=$(git log -1 --pretty=format:"%h")
-if [[ ("$LAST_COMMIT_MSG" == '[maven-release-plugin] prepare release'* ) && ( -z "$LAST_COMMIT_TAG" ) ]] ; then
+if [[ ("$LAST_COMMIT_MSG" == '[maven-release-plugin] prepare release'* ) ]] ; then
   echo 'Skipping build for release preparation step.'
   exit 0
 fi
