6b664199537ffdf269bce45317c9e65e1dded528
Lasantha Kularatne <lkularatne@vrbo.com>
2021-03-31 19:49:37 -0500

    Patching the version

--- release.sh
+++ release.sh
@@ -35,7 +35,7 @@ git commit -m "[npm] prepare release @vrbo/jarviz-graph@${TMP_NPM_VERSION}"
 cd "${JARVIZ_HOME}"
 
 printf '\nPreparing release of jarviz-lib...\n'
-mvn release:prepare -Dresume=false -DskipTests -Darguments='-DskipTests'
+mvn release:prepare release:perform -Dresume=false -DskipTests
 
 echo 'Done'
 
